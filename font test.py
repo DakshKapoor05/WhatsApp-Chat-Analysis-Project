6004eea94f6676a235f492b4503151a67d97dc35
DakshKapoor05 <dakshkapoor05@gmail.com>
2025-07-09 02:17:02 +0530

    Delete font test.py

--- font test.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import matplotlib.font_manager as fm
-
-font_path = r"C:\Users\PC\OneDrive\Desktop\wp-chat-analysis-app\download kara hua font\NotoEmoji-VariableFont_wght.ttf"
-emoji_font = fm.FontProperties(fname=font_path)
-
-# Check font name
-print(emoji_font.get_name())
